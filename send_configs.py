231ea72095f53e3580391da6d19aff1dc2d6d558
aSailorWithoutTheSea <154463231+aSailorWithoutTheSea@users.noreply.github.com>
2025-06-28 07:19:07 +0330

    Update send_configs.py

--- send_configs.py
+++ send_configs.py
@@ -1,23 +1,28 @@
-import requests
 import os
+import requests
 from telegram import Bot
 
-bot_token = os.environ.get("BOT_TOKEN")
-channel_id = os.environ.get("CHANNEL_ID")
+def main():
+    bot_token = os.getenv("BOT_TOKEN")
+    channel_id = os.getenv("CHANNEL_ID")
 
-url = "https://raw.githubusercontent.com/barry-far/V2ray-Config/main/Sub16.txt"
+    url = "https://raw.githubusercontent.com/barry-far/V2ray-Config/main/Sub16.txt"
 
-response = requests.get(url)
-if response.status_code == 200:
-    content = response.text.strip()
-    lines = content.splitlines()
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.text.strip()
+        lines = content.splitlines()
 
-    bot = Bot(token=bot_token)
-    for line in lines:
-        if line.strip():
-            try:
-                bot.send_message(chat_id=channel_id, text=line)
-            except Exception as e:
-                print("خطا در ارسال:", e)
-else:
-    print("خطا در دریافت فایل:", response.status_code)
+        bot = Bot(token=bot_token)
+
+        for line in lines:
+            if line.strip():
+                try:
+                    bot.send_message(chat_id=channel_id, text=line)
+                except Exception as e:
+                    print("خطا در ارسال پیام:", e)
+    else:
+        print("خطا در دریافت فایل کانفیگ:", response.status_code)
+
+if __name__ == "__main__":
+    main()
